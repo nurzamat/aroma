cbf22b365644df397dcd7ed6b3cbfc0cd96995bf
Kanybek <kaniakun@gmail.com>
2019-03-27 13:42:12 +0600

    bonus

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', MainPage, name='MainPage'),
+    path('<int:id>', ProductDetails, name='ProductDetails'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from shop.models import Product
 # Create your views here.
 def MainPage(request):
@@ -6,4 +6,12 @@ def MainPage(request):
     return render(request, 'main/main.html', {
         'products': products
     })
+
+# Страница товара
+def ProductDetails(request, id):
+
+    product = get_object_or_404(Product, id=id, available=True)
+
+    return render(request, 'main/detail.html',
+                  {'product': product})
 
