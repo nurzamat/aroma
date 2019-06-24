c447bb921fda7583e4c51ed59d0bf0c2f436b9d6
NNasyrkanov <nurzamat@gmail.com>
2019-06-24 15:15:52 +0600

    ----

--- aroma/celery.py
+++ aroma/celery.py
@@ -17,3 +17,8 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
 
+
+@app.task(bind=True)
+def bonus_calculation(self):
+    print('Request: {0!r}'.format(self.request))
+
