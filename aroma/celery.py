a77b25cf2a90ee1ec64b9f208b38e73356380ca7
NNasyrkanov <nurzamat@gmail.com>
2019-07-23 00:53:54 +0600

    test

--- aroma/celery.py
+++ aroma/celery.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 import os
 from celery import Celery
+from celery.schedules import crontab
 from django.conf import settings
 
 
@@ -14,6 +15,29 @@ app = Celery('aroma')
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+
+    # Calls test('hello') every 10 seconds.
+    # sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+
+    # Calls test('world') every 30 seconds
+    # sender.add_periodic_task(30.0, test.s('world'), expires=10)
+
+    # Executes every Monday morning at 7:30 a.m.
+    # sender.add_periodic_task(
+    #    crontab(hour=7, minute=30, day_of_week=1),
+    #    test.s('Happy Mondays!'),
+    # )
+
+
+@app.task
+def test(arg):
+    print(arg)
 
 
 @app.task(bind=True)
